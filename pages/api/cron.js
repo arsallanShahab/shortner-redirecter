da674800bf89f5f9911e6b190a66d249a856e5c8
arsallanShahab <mdarsallan@gmail.com>
2023-06-07 21:09:04 +0530

    patch for cors

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -3,10 +3,7 @@ import clientPromise from "@/lib/mongodb";
 export default async function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Cache-Control", "no-cache");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://shortner-cron-server.netlify.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   if (req.method === "POST") {
     const client = await clientPromise;
     const db = client.db();
