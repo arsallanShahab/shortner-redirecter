806eec4425b87fe036fa4597c1db7fd354320800
arsallanShahab <mdarsallan@gmail.com>
2023-06-08 23:26:59 +0530

    used window object to redirect

--- app/[urlID]/page.js
+++ app/[urlID]/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Head from "next/head";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Page({ params }) {
